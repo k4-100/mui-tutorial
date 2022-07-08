a693f1b58b5f28d121ea63f364d20e25331066ca
k4-100 <k4-100@protonmail.com>
2022-07-08 15:25:41 +0200

    Basic button in mui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import SaveIcon from "@mui/icons-material/Save";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          startIcon={<SaveIcon />}
+          onClick={() => alert("hello")}
+          // disabled
+          style={{
+            fontSize: 14,
+          }}
+          variant="contained"
+          color="secondary"
+          size="large"
+          href="#"
+          endIcon={<SaveIcon />}
         >
-          Learn React
-        </a>
+          Hi uss
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
