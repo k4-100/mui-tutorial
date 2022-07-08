8b7044c10f78bd8d555abd450733548439ca49da
k4-100 <k4-100@protonmail.com>
2022-07-08 16:40:59 +0200

    Added ButtonGroups

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import SaveIcon from "@mui/icons-material/Save";
+import DeleteIcon from "@mui/icons-material/Delete";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          startIcon={<SaveIcon />}
-          onClick={() => alert("hello")}
-          // disabled
-          style={{
-            fontSize: 14,
-          }}
-          variant="contained"
-          color="secondary"
-          size="large"
-          href="#"
-          endIcon={<SaveIcon />}
-        >
-          Hi uss
-        </Button>
+        <ButtonGroup variant="contained" size="large">
+          <Button
+            startIcon={<SaveIcon />}
+            onClick={() => alert("hello")}
+            // disabled
+            style={{
+              fontSize: 14,
+            }}
+            color="primary"
+          >
+            Save
+          </Button>
+          <Button
+            startIcon={<DeleteIcon />}
+            onClick={() => alert("hello")}
+            // disabled
+            style={{
+              fontSize: 14,
+            }}
+            color="secondary"
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
