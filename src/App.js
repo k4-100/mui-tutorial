0d43c08461a87c9381330d44588ce111e1048122
k4-100 <k4-100@protonmail.com>
2022-07-08 17:10:51 +0200

    Added CheckboxExample

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import SaveIcon from "@mui/icons-material/Save";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { CheckBox } from "@mui/icons-material";
+import { FormControlLabel } from "@mui/material";
+
+const CheckboxExample = () => {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <FormControlLabel
+      control={
+        <CheckBox
+          checked={checked}
+          icon={<DeleteIcon />}
+          checkedIcon={<SaveIcon />}
+          onChange={(e) => setChecked(e.target.checked)}
+          inputProps={{
+            "aria-label": "secondary checkbox",
+          }}
+        />
+      }
+      label="testing Checkbox"
+    />
+  );
+};
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
         <ButtonGroup variant="contained" size="large">
           <Button
             startIcon={<SaveIcon />}
