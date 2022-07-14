871440ee6ac6ab235962c6cb1a93725e2b870404
k4-100 <k4-100@protonmail.com>
2022-07-14 15:32:42 +0200

    Screwing around with sizing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Grid, Box, Paper } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-import "./App.css";
-
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -17,17 +15,20 @@ function App() {
     <div className="App">
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <Item>xs=8</Item>
+          <Grid item xs={12} sm={4}>
+            <Item>xs=12 sm=4</Item>
           </Grid>
           <Grid item xs={4}>
             <Item>xs=4</Item>
           </Grid>
-          <Grid item xs={4}>
-            <Item>xs=4</Item>
+          <Grid item xs={6} sm={4}>
+            <Item>xs=6 sm={4}</Item>
           </Grid>
-          <Grid item xs={8}>
-            <Item>xs=8</Item>
+          <Grid item xs={2} sm={12}>
+            <Item>xs=2 ms=12</Item>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Item>xs=12 sm=4</Item>
           </Grid>
         </Grid>
       </Box>
