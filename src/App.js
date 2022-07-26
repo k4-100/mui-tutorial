2982bcb8613515775e75c7bacfe5a0214a683487
k4-100 <k4-100@protonmail.com>
2022-07-26 14:37:29 +0200

    dodano nieco textu

--- src/App.js
+++ src/App.js
@@ -2,26 +2,47 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
+import { IconButton, Typography } from "@mui/material";
 import { CameraAlt } from "@mui/icons-material";
+import { grey } from "@mui/material/colors";
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <CameraAlt />
-          </IconButton>
-          <h2>Album Layout</h2>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <CameraAlt />
+            </IconButton>
+            <h2>Album Layout</h2>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Typography
+        align="center"
+        variant="h2"
+        gutterBottom={true}
+        sx={{ mt: 5 }}
+      >
+        Album Layout
+      </Typography>
+      <Typography
+        variant="h6"
+        align="center"
+        // lg={{ mx: 8 }}
+        sx={{ mx: "auto", width: "50%", color: grey[800] }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique vel
+        minus voluptatibus. Mollitia, maxime accusamus error nihil nam possimus
+        impedit?
+      </Typography>
+    </>
   );
 }
