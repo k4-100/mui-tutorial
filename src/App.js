d4ca34123c863501c28f3b7586f7488719b0ba06
k4-100 <k4-100@protonmail.com>
2022-07-29 13:34:14 +0200

    Created barebone structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 import * as React from "react";
+import { LockOutlined } from "@mui/icons-material";
+import {
+  Container,
+  Typography,
+  TextField,
+  Box,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Link,
+} from "@mui/material";
 
 export default function App() {
-  return <h1>words</h1>;
+  return (
+    <>
+      <Container sx={{ mt: 5 }}>
+        <LockOutlined fontSize="large" color="secondary" />
+        <Typography>Sign In</Typography>
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <TextField id="email" label="Email Address *" variant="outlined" />
+          <TextField id="password" label="Password *" variant="outlined" />
+        </Box>
+        <FormControlLabel control={<Checkbox />} label="Remember me" />
+        <Button variant="contained" sx={{ display: "block" }}>
+          Sign in
+        </Button>
+        <Box>
+          <Link href="#">Forgot password</Link>
+          <Link href="#">Don't have an account? Sign Up</Link>
+        </Box>
+        <Typography>
+          Copyright © <Link href="#">Your Website</Link> 2022.
+        </Typography>
+      </Container>
+    </>
+  );
 }
