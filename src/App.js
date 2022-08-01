33ecbffde53485f9dc20c3cac9d915371cff3c34
k4-100 <k4-100@protonmail.com>
2022-08-01 16:58:43 +0200

    Added barebone of the structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { AppBar, Button, Toolbar, Box } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  Grid,
+} from "@mui/material";
 import * as React from "react";
 
 export default function App() {
   return (
     <>
-      <AppBar>
+      <AppBar
+        sx={{
+          position: "static",
+        }}
+      >
         <Toolbar>
           <h3>Company name</h3>
           <Box>
@@ -19,6 +33,92 @@ export default function App() {
           </Box>
         </Toolbar>
       </AppBar>
+      <Typography>Pricing</Typography>
+      <Typography>
+        Quickly build an effective pricing table for your potential customers
+        with this layout. It's built with default MUI components with little
+        customization.
+      </Typography>
+
+      <Grid
+        container
+        sx={{
+          background: "red",
+          mt: 5,
+        }}
+      >
+        <Grid item>
+          <Card>
+            <CardContent>adsada</CardContent>
+            <CardActions>
+              <Button>words</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid>
+          <Card item>
+            <CardContent>adsada</CardContent>
+            <CardActions>
+              <Button>words</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid>
+          <Card item>
+            <CardContent>adsada</CardContent>
+            <CardActions>
+              <Button>words</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item container>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Company</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Typography>Copyright © Your Website 2022.</Typography>
     </>
   );
 }
