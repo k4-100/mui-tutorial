5fa6d3b1c8a7902fc885d9b53eef95504162b24b
k4-100 <k4-100@protonmail.com>
2022-08-03 15:41:56 +0200

    Added nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import * as React from "react";
 
 const App = () => {
-  return <>words words words</>;
+  return (
+    <>
+      <AppBar
+        sx={{
+          background: "white",
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" color="text.primary">
+            Company name
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 export default App;
