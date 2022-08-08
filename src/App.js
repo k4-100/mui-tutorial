553fbd051ee372f57aee03a4c75098a7d5e2e23f
k4-100 <k4-100@protonmail.com>
2022-08-08 17:06:38 +0200

    Added styled component with customTheme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import * as React from "react";
-import { styled } from "@mui/system";
+import { styled, createTheme, ThemeProvider } from "@mui/system";
 
-const MyComponent = styled("div")({
-  color: "darkslategray",
-  backgroundColor: "aliceblue",
-  padding: 8,
-  borderRadius: 4,
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+      contrastText: "white",
+    },
+  },
 });
+
+const MyThemeComponent = styled("div")(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+  backgroundColor: theme.palette.primary.main,
+  padding: theme.spacing(1),
+  borderRadius: theme.shape.borderRadius,
+}));
 
 const App = () => {
   return (
-    <>
-      <MyComponent>text here</MyComponent>
-    </>
+    <ThemeProvider theme={customTheme}>
+      <MyThemeComponent>Styled div with theme</MyThemeComponent>
+    </ThemeProvider>
   );
 };
 
