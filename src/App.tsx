7e0ac1a1f83b154643d64a3c6152e16b02bcf197
k4-100 <k4-100@protonmail.com>
2022-08-11 14:06:42 +0200

    Added nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { Notifications } from "@mui/icons-material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
 
 const App: React.FC = () => {
-  return <>App</>;
+  return (
+    <>
+      <AppBar>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+            }}
+          >
+            Dashboard
+          </Typography>
+          <IconButton>
+            <Notifications
+              sx={{
+                color: "white",
+              }}
+            />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default App;
