10567a752baf6c8aafaa3845d8b7fac2e7b28765
k4-100 <k4-100@protonmail.com>
2022-08-16 13:51:42 +0200

    Installed faker js and used it in simple data representation

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,64 @@ import {
 } from "@mui/material";
 import React from "react";
 
-const drawerWidth = 240;
-const appBarHeight = 64;
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+// export function App() {
+//   return <Bar options={options} data={data} />;
+// }
+
+const drawerWidth: number = 240;
+const appBarHeight: number = 64;
 
 const App: React.FC = () => {
   return (
@@ -162,9 +218,19 @@ const App: React.FC = () => {
               elevation={2}
               sx={{
                 height: "100%",
+                p: "10px",
               }}
             >
-              <Typography>Today</Typography>
+              <Typography
+                color="primary.main"
+                variant="h6"
+                sx={{
+                  fontWeight: "bold",
+                }}
+              >
+                Today
+              </Typography>
+              <Bar options={options} data={data} />
             </Paper>
           </Grid>
           <Grid item xs={4}>
