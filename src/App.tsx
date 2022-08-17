6045260fbcd19e791d3b67c33bbe06b7b8296ae0
k4-100 <k4-100@protonmail.com>
2022-08-17 17:49:52 +0200

    Added first styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,21 @@
-import * as React from "react";
-import { styled, createTheme, ThemeProvider } from "@mui/system";
+import React from "react";
+import styled from "@emotion/styled";
 
-interface MyThemeComponentProps {
-  color?: "primary" | "secondary" | "warning";
-  variant?: "normal" | "dashed";
-}
-
-const customTheme = createTheme({
-  components: {
-    MyThemeComponent: {
-      styleOverrides: {
-        root: {
-          color: "darkslategray",
-        },
-        primary: {
-          color: "darkblue",
-        },
-        secondary: {
-          color: "darkred",
-          background: "pink",
-        },
-        warning: {
-          color: "white",
-          background: "yellow",
-        },
-      },
-      variants: [
-        {
-          props: { variant: "dashed", color: "primary" },
-          style: {
-            border: "1px dashed darkBlue",
-          },
-        },
-        {
-          props: { variant: "dashed", color: "secondary" },
-          style: {
-            border: "1px dashed darkred",
-          },
-        },
-        {
-          props: { variant: "dashed", color: "warning" },
-          style: {
-            border: "1px dashed darkyellow",
-          },
-        },
-      ],
-    },
-  },
-});
-const MyThemeComponent = styled("div", {
-  // Configure which props should be forwarded on DOM
-  shouldForwardProp: (prop) =>
-    prop !== "color" && prop !== "variant" && prop !== "sx",
-  name: "MyThemeComponent",
-  slot: "Root",
-
-  overridesResolver: (props, styles) => [
-    styles.root,
-    props.color === "primary" && styles.primary,
-    props.color === "secondary" && styles.secondary,
-    props.color === "warning" && styles.warning,
-  ],
-})<MyThemeComponentProps>(({ theme }) => ({
-  backgroundColor: "aliceblue",
-  padding: theme.spacing(1),
-}));
+const CustomComponent = styled.div`
+  color: blue;
+  background: green;
+  border-radius: 10px;
+  text-align: center;
+  padding: 1rem;
+  font-size: 20px;
+  width: 300px;
+  margin: 2rem auto;
+`;
 
 const App: React.FC = () => {
   return (
     <>
-      <ThemeProvider theme={customTheme}>
-        <MyThemeComponent sx={{ m: 1 }} color="primary" variant="dashed">
-          Primary
-        </MyThemeComponent>
-        <MyThemeComponent sx={{ m: 1 }} color="secondary">
-          Secondary
-        </MyThemeComponent>
-        <MyThemeComponent sx={{ m: 1 }} color="warning">
-          Warning
-        </MyThemeComponent>
-        <MyThemeComponent sx={{ m: 1 }}>None</MyThemeComponent>
-        words
-      </ThemeProvider>
+      <CustomComponent>words</CustomComponent>
     </>
   );
 };
